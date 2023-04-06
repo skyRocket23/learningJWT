d9d81fc9ca7baeacd9cdc6a6a4f2e3ee586c14fd
skyRocket23 <gitBelwal23@gmail.com>
2023-04-06 21:02:06 +0530

    firstCommit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authroutes = require('./routes/authroutes');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://vbelwal73:Lbvtacmpikuc1@cluster0.dtygzxn.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+
+app.use(authroutes);
---
+++ routes/authroutes.js
@@ -0,0 +1,17 @@
+const {Router} = require('express');
+const authcontrollers = require('../controllers/authcontrollers');
+
+
+
+
+const router = Router();
+
+router.get('/signup',authcontrollers.signup_get)
+
+router.post('/signup',authcontrollers.signup_post)
+
+router.get('/login', authcontrollers.login_get)
+
+router.post('/login',authcontrollers.login_post)
+
+module.exports = router;
